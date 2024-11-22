6c81542a5855c97e5e902553d556587d34039e0d
Daniel Kermany <daniel.kermany@gmail.com>
2024-11-22 12:45:16 -0600

    Changed epoch from 1000 -> 50 and lr from 1e-2 -> 3e-2

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -144,12 +144,12 @@ class nnUNetTrainer(object):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        self.initial_lr = 1e-2
+        self.initial_lr = 3e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
         self.num_iterations_per_epoch = 250
         self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 1000
+        self.num_epochs = 50
         self.current_epoch = 0
         self.enable_deep_supervision = True
 
